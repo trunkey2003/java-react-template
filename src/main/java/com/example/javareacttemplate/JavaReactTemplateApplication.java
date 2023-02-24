bf055d35eb6e04f34e991dd7e4687826ce173d7b
Ho Quang Lam <21521049@gm.uit.edu.vn>
2023-02-24 10:28:45 +0700

    first commit

---
+++ src/main/java/com/example/javareacttemplate/JavaReactTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.javareacttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javareacttemplate/JavaReactTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javareacttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
